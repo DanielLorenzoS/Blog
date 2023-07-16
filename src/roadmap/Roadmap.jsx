330eabb58e528bbed7e100af4111270572de987c
DanielLorenzoS <daniellorenzo47@aragon.unam.mx>
2023-07-16 16:15:07 -0600

    New API Auth

--- src/roadmap/Roadmap.jsx
+++ src/roadmap/Roadmap.jsx
@@ -6,25 +6,30 @@ const Blog = () => {
   return (
     <>
       <div className="publics">
-        <Theme
-          className="theme theme1"
-          title={"Star Wars CRUD JavaScript"}
-          url={"https://tranquil-moxie-09d591.netlify.app/"}
+      <Theme
+          className="theme"
+          title={"PokeAPI Angular & Spring Boot with Authentication & Authorization"}
+          url={"https://pokedex-ang.vercel.app/"}
         />
+        <Theme
+          className="theme"
+          title={"PokeAPI CRUD React & Spring Boot"}
+          url={"https://pokedex-oqxy.vercel.app/"}
+        />
+          <Theme
+            className="theme"
+            title={"PokeAPI CRUD Angular"}
+            url={"https://angpokemons.netlify.app/"}
+          />
         <Theme
           className="theme"
           title={"First Portfolio"}
           url={"https://daniellorenzos.github.io/Portafolio/"}
         />
         <Theme
-          className="theme"
-          title={"PokeAPI CRUD Angular"}
-          url={"https://angpokemons.netlify.app/"}
-        />
-        <Theme
-          className="theme"
-          title={"PokeAPI CRUD React & Spring Boot"}
-          url={"https://pokedex-oqxy.vercel.app/"}
+          className="theme theme1"
+          title={"Star Wars CRUD JavaScript"}
+          url={"https://tranquil-moxie-09d591.netlify.app/"}
         />
       </div>
     </>
