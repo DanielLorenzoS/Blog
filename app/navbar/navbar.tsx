a56df6a871cea99715efe9cb483126fcfda5db8a
DanielLorenzoS <daniellorenzo47@aragon.unam.mx>
2023-09-21 00:03:05 -0600

    Change to Nextjs

---
+++ app/navbar/navbar.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./navbar.module.css";
+import React, { useState } from "react";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import logo from "../../public/loredev-bg-grey.png";
+
+export default function Navbar() {
+
+    const [buttonClicked, setButtonClicked] = useState(false);
+
+    const handleButtonClick = () => {
+        setButtonClicked(!buttonClicked);
+        if (!buttonClicked) {
+            disableBodyScroll(document.body);
+        } else {
+            enableBodyScroll(document.body);
+        }
+    };
+
+    return (
+        <>
+            <nav className={styles.nav}>
+                <h1 className={styles["nav-h1"]}>
+                    LoreDev</h1>
+                <div className={styles.navs}>
+                    <Link href="/">
+                        <h2 className={styles["nav-h2"]}>Inicio</h2>
+                    </Link>
+                    <Link href="/about">
+                        <h2 className={styles["nav-h2"]}>Sobre mí</h2>
+                    </Link>
+                    <Link href="/products">
+                        <h2 className={styles["nav-h2"]}>Proyectos</h2>
+                    </Link>
+                    <Link href="/contact">
+                        <h2 className={styles["nav-h2"]}>Contacto</h2>
+                    </Link>
+                </div>
+                <button
+                    className={
+                        buttonClicked
+                            ? `${styles["nav-res-button"]} ${styles["nav-res-button-clicked"]}`
+                            : styles["nav-res-button"]
+                    }
+                    onClick={handleButtonClick}
+                >
+                    <div
+                        className={
+                            buttonClicked
+                                ? `${styles.image} ${styles["image-clicked"]}`
+                                : styles.image
+                        }
+                    ></div>
+                    <div
+                        className={
+                            buttonClicked
+                                ? `${styles.image2} ${styles["image2-clicked"]}`
+                                : styles.image2
+                        }
+                    ></div>
+                    <div
+                        className={
+                            buttonClicked
+                                ? `${styles.image3} ${styles["image3-clicked"]}`
+                                : styles.image3
+                        }
+                    ></div>
+                </button>
+                <div
+                    className={
+                        buttonClicked ? styles["nav-res-clicked"] : styles["nav-res"]
+                    }
+                    onClick={handleButtonClick}
+                >
+                    <Link href="/">
+                        <h2 className={styles["nav-h2"]}>Home</h2>
+                    </Link>
+                    <Link href="/roadmap">
+                        <h2 className={styles["nav-h2"]}>Projects</h2>
+                    </Link>
+                    <Link href="/projects">
+                        <h2 className={styles["nav-h2"]}>Experience</h2>
+                    </Link>
+                    <Link href="/contact">
+                        <h2 className={styles["nav-h2"]}>Contact</h2>
+                    </Link>
+                </div>
+            </nav>
+
+        </>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import info from '../public/info.svg'
+import productos from '../public/productos.svg'
+import contacto from '../public/contact.svg'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.welcome}>
+          <h1 className={styles.title}>
+            Bienvenid@ a <span className={styles.blue}>LoreDev</span>
+          </h1>
+          <h2 className={styles.subtitle}>
+          ¿List@ para dar vida a tu <span className={styles.blue}>proyecto</span> en línea?
+          </h2>
+        </div>
+        <div className={styles.info}>
+          <Link href="/about" className={styles.card}>
+            Acerca de mí
+            <Image src={info} width={50} height={50} alt='info' className={styles.icons} />
+          </Link>
+          <Link href="/products" className={styles.card}>
+            Proyectos
+            <Image src={productos} width={50} height={50} alt='info' className={styles.icons} />
+          </Link>
+          <Link href="/contact" className={styles.card}>
+            Contacto
+            <Image src={contacto} width={50} height={50} alt='info' className={styles.icons} />
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
